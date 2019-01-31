93f58ba7f7e4f10c7b69720d0ed2014415fe3f90
Maciej Rudnicki <maciejrudnicki11@gmail.com>
2019-01-31 11:42:04 +0100

    feat : initial commit

---
+++ src/main/java/com/example/demo/SampleService.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleService {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+}
